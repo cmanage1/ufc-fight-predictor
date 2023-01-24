b165166303ab9a0007481cf3a50a2c15368846b3
cmanage1 <chethinlm@gmail.com>
2023-01-23 22:51:18 -0500

    Make MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
-import React, {useEffect, useState} from 'react';
-import { Box, MenuItem, Button, FormControl, Select, InputLabel, Grid, Paper } from '@mui/material';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  MenuItem,
+  Button,
+  FormControl,
+  Select,
+  InputLabel,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
+import Link from "@mui/material/Link";
 
 function App() {
   const [fightersList, setFightersList] = useState([]);
-  const [fighter1, setFighter1 ] = useState('');
-  const [fighter2, setFighter2 ] = useState('');
+  const [fighter1, setFighter1] = useState("");
+  const [fighter2, setFighter2] = useState("");
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
   const [calculated, setCalculated] = useState(false);
+  const [odds, setOdds] = useState("");
 
   const handleChange1 = (event) => {
     setFighter1(event.target.value);
@@ -19,159 +31,147 @@ function App() {
 
   useEffect(() => {
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        },
-        mode: 'cors'
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      mode: "cors",
     };
 
-    fetch("http://127.0.0.1:5000/get/ranked_fighter_names", options)
-    .then(res => res.json())
-    .then(
-      (data) => {
-        setLoaded(true);
-        setFightersList(data);
-      },
-      (error) => {
-        setLoaded(true);
-        setError(error);
-      }
-    )
-  }, [])
+    fetch("http://127.0.0.1:8000/get/all_fighters", options)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setLoaded(true);
+          setFightersList(data);
+        },
+        (error) => {
+          setLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
   const calculateOdds = () => {
-    const body = {
-      "fighter1" : fighter1, 
-      "fighter2" : fighter2
-    }
+    const s = "r_fighter=" + fighter1 + "&b_fighter=" + fighter2;
+    s.split(" ").join("+");
 
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
+    fetch("http://127.0.0.1:8000/get/odds/?" + s)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setLoaded(true);
+          setCalculated(true);
+          console.log(data);
+          data = JSON.stringify(data);
+          setOdds(data);
         },
-        mode: 'cors',
-        body: JSON.parse(body),
-    };
+        (error) => {
+          setLoaded(true);
+          setError(error);
+        }
+      );
+  };
 
-    fetch("http://127.0.0.1:5000/post/calculate_odds", options)
-    .then(res => res.json())
-    .then(
-      (data) => {
-        setCalculated(true);
-        // Set odds 
-        console.log(fightersList)
-      },
-      (error) => {
-        setCalculated(true);
-        setError(error);
-      }
-    )
-  }
-
-  if (error){
-    return <div> Error: {error.message}</div>
-  } else if (!loaded){
-    return <div> loading ... </div>
+  if (error) {
+    return <div> Error: {error.message}</div>;
+  } else if (!loaded) {
+    return <div> loading ... </div>;
   } else {
     return (
       <div className="App">
         <Paper>
-        <Box sx={{ flexGrow: 1, padding: "20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Box>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Figher 1</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={fighter1}
-                  label="Fighter 2"
-                  onChange={handleChange1}
-                  MenuProps={{
-                  anchorOrigin: {
-                    vertical: "bottom",
-                    horizontal: "left"
-                  },
-                  transformOrigin: {
-                    vertical: "top",
-                    horizontal: "left"
-                  },
-                  getContentAnchorEl: null,
-                  maxHeight: "50px"
-                  //MenuProps.PaperProps.style.maxHeight: "0",
-            }}
-                >
-                  {fightersList.map((fighter, i) => {
-                    return(
-                      <MenuItem
-                        value={fighter}
-                        key={i}
-                        >
-                        {fighter} 
-                      </MenuItem>
-                    )
-                    })
-                  }
-                </Select>
-              </FormControl>
-            </Box>
-          </Grid>
+          <Box sx={{ flexGrow: 1, padding: "20px" }}>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <Box>
+                  <Typography variant="subtitle1">Red Corner</Typography>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Fighter 1
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={fighter1}
+                      label="Fighter 2"
+                      onChange={handleChange1}
+                      MenuProps={{
+                        maxheight: "10px",
+                      }}
+                    >
+                      {fightersList.map((fighter, i) => {
+                        return (
+                          <MenuItem value={fighter} key={i}>
+                            {fighter}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Grid>
 
-          <Grid item xs={4}>
-            <Button 
-              variant="contained"
-              disabled={!fighter1 || !fighter2}
-              onClick={calculateOdds}
-            >
-              See odds
-            </Button> 
-            
-            {
-              calculated ? 
-              <div className='odds'> 
-                Winning change : 
+              <Grid item xs={4}>
+                <Box textAlign="center">
+                  <Button
+                    variant="contained"
+                    disabled={!fighter1 || !fighter2}
+                    onClick={calculateOdds}
+                  >
+                    See odds
+                  </Button>
+                  {calculated ? (
+                    <div className="odds">Prediction :{odds}</div>
+                  ) : (
+                    ""
+                  )}
+                </Box>
+              </Grid>
 
-              </div> 
-              
-              : ''
-            }
-          </Grid>
-          
-          <Grid item xs={4}>
-            <Box>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Figher 2</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={fighter2}
-                  label="Figher 2"
-                  onChange={handleChange2}
-                >
-                  {fightersList.map((fighter, i) => {
-                    return(
-                      <MenuItem
-                        value={fighter}
-                        key={i}
-                        >
-                        {fighter} 
-                      </MenuItem>
-                    )
-                    })
-                  }
-                </Select>
-              </FormControl>
-            </Box>
-          </Grid>
-
-        </Grid>
-        </Box>
+              <Grid item xs={4}>
+                <Box>
+                  <Typography variant="subtitle1">Blue Corner</Typography>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Fighter 2
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={fighter2}
+                      label="Figher 2"
+                      onChange={handleChange2}
+                      MenuProps={{
+                        maxheight: "10px",
+                      }}
+                    >
+                      {fightersList.map((fighter, i) => {
+                        return (
+                          <MenuItem value={fighter} key={i}>
+                            {fighter}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box>
+            <Typography variant="body">
+              Odds looking "odd"? <br></br>
+              <br></br>
+              Remember that Red Corner is the defending champion (or the higher
+              rank fighter) and blue is the challenger. Try switching sides
+            </Typography>
+          </Box>
         </Paper>
+        <Box>
+          <Link href="#">Source Code</Link>
+        </Box>
       </div>
     );
   }
