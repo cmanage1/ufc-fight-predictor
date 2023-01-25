24a1a43f3bd5175c068678688f0735e535f1ec37
cmanage1 <chethinlm@gmail.com>
2023-01-24 20:26:30 -0500

    changed to deplyment syntax

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ function App() {
       mode: "cors",
     };
 
-    fetch("http://127.0.0.1:8000/get/all_fighters", options)
+    fetch("../get/all_fighters", options)
       .then((res) => res.json())
       .then(
         (data) => {
@@ -58,7 +58,7 @@ function App() {
     const s = "r_fighter=" + fighter1 + "&b_fighter=" + fighter2;
     s.split(" ").join("+");
 
-    fetch("http://127.0.0.1:8000/get/odds/?" + s)
+    fetch("../get/odds/?" + s)
       .then((res) => res.json())
       .then(
         (data) => {
